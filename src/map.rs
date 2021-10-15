b42eb6ce9ef234f43159a8913671676bc2967dbb
Canaan <cjmckenz@uvm.edu>
2021-10-15 14:01:20 -0400

    randomly generated corridors connecting rooms

--- src/map.rs
+++ src/map.rs
@@ -68,6 +68,19 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType>{
 		}
 		if ok {
 			apply_room_to_map(&new_room, &mut map);
+
+			if !rooms.is_empty(){
+				let (new_x, new_y) = new_room.center();
+				let (prev_x, prev_y) =  rooms[rooms.len()-1].center();
+				if rng.range(0,2) == 1 {
+					apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
+					apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+				} else {
+					apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
+					apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+				}
+			}
+			
 			rooms.push(new_room);
 		}
 	}
@@ -113,7 +126,7 @@ pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
 				ctx.set(x,y, RGB::from_f32(0.5,0.5,0.5),RGB::from_f32(0.,0.,0.), rltk::to_cp437('◙'));
 			}
 			TileType::Wall => {
-				ctx.set(x,y, RGB::from_f32(0.0,1.0,0.0), RGB::from_f32(0.,0.,0.), rltk::to_cp437('▓'));
+				ctx.set(x,y, RGB::from_f32(1.0,4.0,4.0), RGB::from_f32(0.,0.,0.), rltk::to_cp437('▓'));
 			}
 		}
 
