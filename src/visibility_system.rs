1b8ab62c940e10753489e007dc76cd8d6489810f
Canaan <cjmckenz@uvm.edu>
2021-10-16 13:32:38 -0400

    moved memory tracking to visibility_system.rs, error at boundary, crashes

--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -6,17 +6,27 @@ use rltk::{field_of_view,Point};
 pub struct VisibilitySystem {}
 
 impl <'a> System <'a> for VisibilitySystem {
-	type SystemData = (ReadExpect<'a, Map>,
+	type SystemData = (WriteExpect<'a, Map>,
+					   Entities<'a>,
 					   WriteStorage<'a, Viewshed>,
-					   WriteStorage<'a, Position>);
+					   WriteStorage<'a, Position>,
+					   ReadStorage<'a, Player>);
 
 	fn run(&mut self, data : Self::SystemData){
-		let (map, mut viewshed, pos) = data;
+		let (mut map, entities, mut viewshed, pos, player) = data;
 
-		for (viewshed,pos) in (&mut viewshed, &pos).join(){
+		for (ent, viewshed,pos) in (&entities,&mut viewshed,&pos).join(){
 			viewshed.visibile_tiles.clear();
 			viewshed.visibile_tiles = field_of_view(Point::new(pos.x,pos.y), viewshed.range,&*map); //field_of_view is rltk function
 			viewshed.visibile_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >=0 && p.y < map.height);
+
+			let p: Option<&Player> = player.get(ent);
+			if let Some(p) = p {
+				for vis in viewshed.visibile_tiles.iter(){
+					let idx = map.xy_idx(vis.x,vis.y);
+					map.revealed_tiles[idx] = true;
+				}
+			}
 		}
 	}
 }
