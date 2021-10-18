2a34f68968c7154152e5a52d93ff95dc8834f199
Canaan <cjmckenz@uvm.edu>
2021-10-18 17:36:03 -0400

    somewhat cleaner died, console log..goes on forever, TODO add graceful exit

--- src/damage_system.rs
+++ src/damage_system.rs
@@ -1,5 +1,6 @@
 use specs::prelude::*;
-use super::{CombatStats, SufferDamage};
+use super::{CombatStats, SufferDamage,Player};
+use rltk::{console};
 
 pub struct DamageSystem{}
 
@@ -22,11 +23,18 @@ impl <'a> System <'a> for DamageSystem{
 		//use scope to make borrow checker happy
 		{
 			let combat_stats = ecs.read_storage::<CombatStats>();
+			let players = ecs.read_storage::<Player>();
 			let entities =  ecs.entities();
 			for (entity, stats) in (&entities, &combat_stats).join(){
-				if stats.hp < 1 {dead.push(entity);}
+				if stats.hp < 1 {
+					let player = players.get(entity);
+					match player {
+						None => dead.push(entity),
+						Some(_) =>console::log("You Died")
+				 }
 			}
 		}
+	}
 
 		for victim in dead {
 			ecs.delete_entity(victim).expect("Unable to delete");
